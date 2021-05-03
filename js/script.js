142db17da71a87f274f863c6dad826c74b366de0
patrickzw1 <patrickzhenwu@gmail.com>
2021-05-03 22:32:11 +0800

    Create script.js
    use the function of js file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var options = {
+		strings: ['学生', '游戏爱好者'],
+  		typeSpeed: 90,
+  		loop: true,
+  		startDelay: 1000,
+  		showCursor: false,
+  		fadeOut: true,
+	};
+
+	var typed = new Typed('.typed', options);
+});
