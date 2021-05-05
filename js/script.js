f62afcff0d00a71bb9118887ca934ae252d73c2c
patrickzw1 <patrickzhenwu@gmail.com>
2021-05-05 11:54:41 +0800

    active skill section and set it scroll to specific place to start the animation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
+	// activing superslides for top image to slide
 	$('#slides').superslides({
 		animation: 'fade',
 		play: 5000,
 		pagination: false
 	});
 
+	// creating the typed effect of the words
 	var options = {
 		strings: ['学生', '游戏爱好者'],
   		typeSpeed: 90,
@@ -16,4 +18,43 @@ $(document).ready(function(){
 	};
 
 	var typed = new Typed('.typed', options);
+
+	// activing owl carousel for scrolling left or right of skill section
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	$(window).scroll(function(){
+		if (window.pageYOffset > skillsTopOffset - $(window).height() + 100){
+			$('.chart').easyPieChart({
+			    barColor: "#fff",
+			    trackColor: false,
+			    scaleColor: false,
+			    easing: "easeInOut",
+			    size: 152,
+			    lineWidth: 4,
+			    onStep: function(from, to, percent){
+			    	$(this.el).find(".percent").text(Math.round(percent));
+			    }
+			});
+		}
+	});
+
+	
 });
